fcfc1a30cc1ef10373b79cf7837d13fcc41a44d3
Lleafll <Lleafll@users.noreply.github.com>
2015-12-26 00:12:14 +0100

    Update VoidRaidSetup.lua

--- VoidRaidSetup.lua
+++ VoidRaidSetup.lua
@@ -1,3 +1,6 @@
-local addonName, addonTable = ...
-VoidRaidSetup = LibStub("AceAddon-3.0"):NewAddon(addonTable, addonName, "AceEvent-3.0")
+local addonName, VRS = ...
+LibStub("AceAddon-3.0"):NewAddon(VRS, addonName, "AceEvent-3.0")
 
+function VRS:OnInitialize()
+  self.db = LibStub("AceDB-3.0"):New(addonName.."DB", nil, true)
+end
