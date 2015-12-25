4fb4c50064367eca00d60bd2161d94ec9f773dbb
Lleafll <Lleafll@users.noreply.github.com>
2015-12-26 00:23:18 +0100

    Update Import.lua

--- Import.lua
+++ Import.lua
@@ -1,2 +1,27 @@
-local addonName, addonTable = ...
+local addonName, VRS = ...
 
+local function createImportFrame(self)
+  self.ImportFrame = CreateFrame("EditBox", addonName.."Import", UIParent, "ChatFrameEditBoxTemplate")
+end
+
+function VRS:OpenImportFrame()
+  if not self.ImportFrame then
+    createImportFrame(self)
+  end
+  self.ImportFrame:Show()
+end
+
+function VRS:Import()
+  if not self.ImportFrame:IsShown() then
+    return
+  end
+  local input = self.ImportFrame:GetText()
+  local inputFunc = loadstring("return "..input)
+  local inputTable = inputFunc()
+  if not type(inputTable) == "table" then
+    -- raise error message
+  else
+    self.db = inputTable
+    self.ImportFrame:Close()
+  end
+end
