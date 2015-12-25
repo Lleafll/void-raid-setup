8cc03158549712a0a02ffa64d8d55450bd15fe96
Lleafll <Lleafll@users.noreply.github.com>
2015-12-26 00:56:10 +0100

    Update Core.lua

--- Core.lua
+++ Core.lua
@@ -1,8 +1,13 @@
-local addonName, addonTable = ...
+local addonName, VRS = ...
 
 --[[needed events:
   RAID_ROSTER_UPDATE
   GROUP_ROSTER_UPDATE?
   GROUP_JOINED?
+  when changing boss
+  when killing preceding boss?
 ]]--
 
+local function createVRSFrame(self)
+  self.Frame = CreateFrame()  -- create frame for addon functionality here
+end
