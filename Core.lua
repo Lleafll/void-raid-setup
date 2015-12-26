06b9367fe0b871604e1cab6ef5a1416bbbbac915
Lleafll <Lleafll@users.noreply.github.com>
2015-12-26 02:34:36 +0100

    Update Core.lua

--- Core.lua
+++ Core.lua
@@ -1,5 +1,8 @@
 local addonName, VRS = ...
 
+local setup = {}
+local standby = {}
+
 --[[needed events:
   RAID_ROSTER_UPDATE
   GROUP_ROSTER_UPDATE?
@@ -8,6 +11,30 @@ local addonName, VRS = ...
   when killing preceding boss?
 ]]--
 
+local function createPlayer(parent)
+  
+end
+
 local function createVRSFrame(self)
-  self.Frame = CreateFrame()  -- create frame for addon functionality here
+  self.Frame = CreateFrame()  -- create frame for addon functionality
+  self.Frame.setup = {}  -- players which need to be moved into the raid
+  self.Frame.standby = {}  -- players which need to be moved out of the raid
+end
+
+function VRS:UpdateVRS()
+  local bossTable = self.db[self.selectedBoss]
+  for i in NumGroupMembers() do
+    local = name, rank, subgroup, level, class, fileName, zone, online, isDead, role, isML = GetRaidRosterInfo(i)
+    if subgroup <= 4 then
+      if bossTable.setup.standby[name] then
+        standby[#standby+1] = name
+      end
+    else
+      if bossTable.setup.setup[name] then
+        setup[#setup+1] = name
+      end
+    end
+  end
+  
+  
 end
