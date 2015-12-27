75d0ce23beb9ac68e34ed185abdb63b72fca81f5
Lleafll <Lleafll@users.noreply.github.com>
2015-12-27 01:46:20 +0100

    Update VoidRaidSetup.lua

--- VoidRaidSetup.lua
+++ VoidRaidSetup.lua
@@ -4,7 +4,6 @@ LibStub("AceAddon-3.0"):NewAddon(VRS, addonName, "AceEvent-3.0")
 function VRS:HandleChatCommand(command)
   self:OpenImportFrame()
 end
-
 VRS:RegisterChatCommand("vrs", "HandleChatCommand")
 VRS:RegisterCharCommand("voidraidsetup", "HandleChatCommand")
 
