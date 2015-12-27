e25f0b11b138dfa70694b84d99747a9ef729bdf7
Lleafll <Lleafll@users.noreply.github.com>
2015-12-27 01:58:42 +0100

    Update Core.lua

--- Core.lua
+++ Core.lua
@@ -2,19 +2,37 @@ local addonName, VRS = ...
 
 local raidSize = 4  -- number of groups in raid
 
---[[needed events:
-  RAID_ROSTER_UPDATE
-  GROUP_ROSTER_UPDATE?
-  GROUP_JOINED?
-  when changing boss
-  when killing preceding boss?
-]]--
-
 local function createVRSFrame(self)
   self.Frame = CreateFrame()  -- create frame for addon functionality
   self.Frame:SetPoint("TOP", "LEFT")
-  self.Frame.Dropdown:Hide()
-
+  self.Frame:Hide()
+  function self.Frame:Update()
+    local setupString = "Move to Setup:\n"
+    local standbyString = "Move to Standby:\n"
+    self.db.selectedBoss = self.db.selectedBoss or 1
+    local bossTable = self.db.bosses[self.db.selectedBoss]
+    for i in NumGroupMembers() do
+      local name, rank, subgroup, level, class, fileName, zone, online, isDead, role, isML = GetRaidRosterInfo(i)
+      local playerKey = self.db.keys[name]  -- might need to split server string
+      local playerInSetup = bossTable.setup[playerKey]
+      if subgroup <= raidSize and not playerInSetup then
+        standbyString = standbyString .. name .. "\n"
+      elseif subgroup > raidSize and playerInSetup then
+        setupString = setupString .. name .. "\n"
+      end
+    end
+    -- set self.db.name as VRS Frame title
+    self.Frame.Setup:SetText(setupString)
+    self.Frame.Standby:SetText(standbyString)
+  end
+  self.Frame:RegisterEvent("OnShow", "Update")
+  --[[needed events:
+    RAID_ROSTER_UPDATE
+    GROUP_ROSTER_UPDATE?
+    GROUP_JOINED?
+    when killing preceding boss?
+  ]]--
+  
   self.Frame.Dropdown = CreateFrame("Button", self.Frame, "UIDropDownMenuTemplate")
   self.Frame.Dropdown:SetPoint("TOP")
   self.Frame.Dropdown:Show()
@@ -27,7 +45,7 @@ local function createVRSFrame(self)
         local id = self:GetID()
         UIDropDownMenu_SetSelectedID(VRS.Frame.Dropdown, id)
         VRS.db.selectedBoss = id
-        VRS:Update()
+        VRS.Frame:Update()
       end
       UIDropDownMenu_AddButton(info, level)
     end
@@ -47,32 +65,9 @@ local function createVRSFrame(self)
 
   self.Frame.Auto = CreateFrame("Button", self.Frame)  -- look for appropriate button template
   self.Frame.Auto:SetPoint("BOTTOM")
+  self.Frame.Auto:SetText("Auto Sort")
   self.Frame.Auto:Show()
-  self.Frame.Auto:RegisterEvent("OnClick", "AutoSort")
-end
-
-function VRS:Update()
-  local setupString = "Move to Setup:\n"
-  local standbyString = "Move to Standby:\n"
-  
-  self.db.selectedBoss = self.db.selectedBoss or 1
-  local bossTable = self.db.bosses[self.db.selectedBoss]
-  for i in NumGroupMembers() do
-    local name, rank, subgroup, level, class, fileName, zone, online, isDead, role, isML = GetRaidRosterInfo(i)
-    local playerKey = self.db.keys[name]  -- might need to split server string
-    local playerInSetup = bossTable.setup[playerKey]
-    if subgroup <= raidSize and not playerInSetup then
-      standbyString = standbyString .. name .. "\n"
-    elseif subgroup > raidSize and playerInSetup then
-      setupString = setupString .. name .. "\n"
-    end
-  end
-  
-  -- set self.db.name as VRS Frame title
-  self.Frame.Setup:SetText(setupString)
-  self.Frame.Standby:SetText(standbyString)
-end
-
-function VRS:AutoSort()
-  
+  self.Frame.Auto:RegisterEvent("OnClick", function()
+    -- Auto Sort
+  end)
 end
