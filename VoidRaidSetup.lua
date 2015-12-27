93acaa555da2274ea65f4bb1d8478f48175b1149
Lleafll <Lleafll@users.noreply.github.com>
2015-12-27 01:45:59 +0100

    Update VoidRaidSetup.lua

--- VoidRaidSetup.lua
+++ VoidRaidSetup.lua
@@ -1,6 +1,17 @@
 local addonName, VRS = ...
 LibStub("AceAddon-3.0"):NewAddon(VRS, addonName, "AceEvent-3.0")
 
+function VRS:HandleChatCommand(command)
+  self:OpenImportFrame()
+end
+
+VRS:RegisterChatCommand("vrs", "HandleChatCommand")
+VRS:RegisterCharCommand("voidraidsetup", "HandleChatCommand")
+
 function VRS:OnInitialize()
-  self.db = LibStub("AceDB-3.0"):New(addonName.."DB", nil, true)
+  self.db = LibStub("AceDB-3.0"):New(addonName.."DB", {
+    name = "",
+    keys = {},
+    bosses = {}
+  }, true)
 end
